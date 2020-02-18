838615e03c32f261faa52b782fe1f6f59571148b
Vision Hung <scps950613@gmail.com>
2020-02-19 00:15:17 +0800

    commit a dog image on about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img width="500" src="https://images.unsplash.com/photo-1507146426996-ef05306b995a" alt="dog">
   </div>
 </template>
